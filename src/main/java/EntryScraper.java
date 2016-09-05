14858202afbfb57b31ace390d858da444af7e984
Sait Samet Karakaya <sskarakaya@msn.com>
2016-09-05 13:20:02 +0300

    Entry Scraper eklendi

--- src/main/java/EntryScraper.java
+++ src/main/java/EntryScraper.java
@@ -23,10 +23,19 @@ public class EntryScraper {
             dailyBundle.setDate(LocalDate.now().toString());
             dailyBundle.setWebsite("eksisozluk.com");
             for (Element element : debeList) {
-                userAgent.visit(element.getFirst("<a/>").getAt("href"));
-                Element content = userAgent.doc.findFirst("<div class='content'");
+                String link = element.getFirst("<a/>").getAt("href");
+                userAgent.visit(link);
+                Element title = userAgent.doc.findFirst("<span itemprop='name'");
+                Element body = userAgent.doc.findFirst("<div class='content'");
+                Element entryDate = userAgent.doc.findFirst("<a class='entry-date permalink'");
+                Element author = userAgent.doc.findFirst("<a class='entry-author'");
                 Entry entry = new Entry();
-                entry.setBody(content.getText());
+                entry.setTitle(title.getText());
+                entry.setBody(body.getText());
+                entry.setEntryDate(entryDate.getText());
+                entry.setAuthor(author.getText());
+                entry.setWebsite("eksisozluk");
+                entry.setWebLink(link);
                 dailyBundle.getEntryList().add(entry);
             }
 
